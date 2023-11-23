368775b8be6df4702d89912b99211f4794720f54
Alek-sas-hka <103955957+Alek-sas-hka@users.noreply.github.com>
2023-11-23 23:58:31 +0300

    Update __init__.py

--- scr/__init__.py
+++ scr/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.secret_key = 'fortinaiti pabigi'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -12,5 +11,13 @@ manager = LoginManager(app)
 
 from src import routes
 
+# Функция для создания таблиц в контексте приложения
+def create_tables():
+    with app.app_context():
+        db.create_all()
 
-db.create_all()
+# Вызываем функцию для создания таблиц
+create_tables()
+
+if __name__ == "__main__":
+    app.run(debug=True)
